74ad3355b67027fe742edaf2b9ba136367fe6966
沈鑫 <1683137709@qq.com>
2023-11-24 09:05:18 +0800

    transform完善

--- Runtime/Components/Transform.cpp
+++ Runtime/Components/Transform.cpp
@@ -1,59 +1,107 @@
 #include "Transform.h"
+#include <glm/gtx/matrix_decompose.hpp>
 
 Transform::Transform()
     : _translation(0.f),
       _rotation(1.f, 0.f, 0.f, 0.f),
-      _scale(0.f),
+      _scale(1.f),
       _pParent(nullptr),
+      _worldTranslation(0.f),
+      _worldRotation(1.f, 0.f, 0.f, 0.f),
+      _worldScale(1.f),
       _matLocal(glm::identity<glm::mat4x4>()),
       _matWorld(glm::identity<glm::mat4x4>()),
       _matInvLocal(glm::identity<glm::mat4x4>()),
-      _invMatWorld(glm::identity<glm::mat4x4>()),
+      _matInvWorld(glm::identity<glm::mat4x4>()),
       _dirtyFlag() {
+    _dirtyFlag = SetFlags(_dirtyFlag, eAllDirty);
+}
 
-    SetFlags(_dirtyFlag, eAllDirty);
+Transform::~Transform() {
+    if (_pParent != nullptr) {
+		SetParent(nullptr);
+    }
+    for (auto it = _children.begin(); it != _children.end(); ++it) {
+        auto *pChild = *it;
+        pChild->_pParent = nullptr;
+        pChild->_dirtyFlag = SetFlags(pChild->_dirtyFlag, eWorldAttribute);
+        _children.erase(it);
+    }
 }
 
 void Transform::SetLocalPosition(const glm::vec3 &translate) {
     if (any(epsilonNotEqual(_translation, translate, kEpsilon))) {
         _translation = translate;
-        SetFlags(_dirtyFlag, eAllDirty);
+        _dirtyFlag = SetFlags(_dirtyFlag, eAllDirty);
+        MakeChildrenDirty(eWorldAttribute);
     }
 }
 
 void Transform::SetLocalScale(const glm::vec3 &scale) {
     if (any(epsilonNotEqual(_scale, scale, kEpsilon))) {
         _scale = scale;
-        SetFlags(_dirtyFlag, eAllDirty);
+        _dirtyFlag = SetFlags(_dirtyFlag, eAllDirty);
+        MakeChildrenDirty(eWorldAttribute);
     }
 }
 
-void Transform::SetLocalRotate(const glm::quat &rotate) {
+void Transform::SetLocalRotation(const glm::quat &rotate) {
     if (any(epsilonNotEqual(_rotation, rotate, kEpsilon))) {
         _rotation = rotate;
-        SetFlags(_dirtyFlag, eAllDirty);
+        _dirtyFlag = SetFlags(_dirtyFlag, eAllDirty);
+        MakeChildrenDirty(eWorldAttribute);
     }
+}
+
+void Transform::SetLocalMatrix(const glm::mat4x4 &matrix) {
+    _matLocal = matrix;
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(_matLocal, _scale, _rotation, _translation, skew, perspective);
+    _dirtyFlag = ClearFlags(_dirtyFlag, eLocalMatrix);
+    _dirtyFlag = SetFlags(_dirtyFlag, eInverseLocalMatrix | eWorldAttribute);
+    MakeChildrenDirty(eWorldAttribute);
+}
+
+void Transform::SetWorldMatrix(const glm::mat4x4 &matrix) {
+    _matWorld = matrix;
+
+    glm::mat4x4 parentInvWorld = glm::identity<glm::mat4x4>();
+    if (_pParent != nullptr) {
+        parentInvWorld = _pParent->GetInverseWorldMatrix();
+    }
+    _matLocal = matrix * parentInvWorld;
+
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(_matLocal, _scale, _rotation, _translation, skew, perspective);
+
+    MakeChildrenDirty(eWorldAttribute);
+    _dirtyFlag = SetFlags(_dirtyFlag, eInverseLocalMatrix | eWorldAttribute);
+    _dirtyFlag = ClearFlags(_dirtyFlag, eWorldMatrix | eLocalMatrix);
 }
 
 void Transform::SetParent(Transform *pTransform) {
     if (_pParent != pTransform) {
-	    if (_pParent != nullptr) {
-		    RemoveChildImpl(_pParent, this);
-	    }
+        if (_pParent != nullptr) {
+            RemoveChildImpl(_pParent, this);
+        }
         SetParentImpl(pTransform, this);
-        SetFlags(_dirtyFlag, eWorldMatrix | eInverseWorldMatrix);
+        MakeChildrenDirty(eWorldAttribute);
+        _dirtyFlag = SetFlags(_dirtyFlag, eWorldAttribute);
     }
 }
 
 void Transform::AddChild(Transform *pTransform) {
     if (pTransform->GetParent() != this) {
-		pTransform->SetParent(this);
+        pTransform->SetParent(this);
     }
 }
 
 void Transform::RemoveChild(Transform *pTransform) {
     if (pTransform->GetParent() == this) {
-	    RemoveChildImpl(this, pTransform);
+        RemoveChildImpl(this, pTransform);
+        _dirtyFlag = SetFlags(pTransform->_dirtyFlag, eWorldAttribute);
     }
 }
 
@@ -62,7 +110,7 @@ auto Transform::GetLocalMatrix() const -> const glm::mat4x4 & {
         // TRS
         _matWorld = glm::scale(glm::mat4_cast(_rotation), _scale);
         _matWorld = glm::translate(glm::identity<glm::mat4>(), _translation) * _matWorld;
-        ClearFlags(_dirtyFlag, eLocalMatrix);
+        _dirtyFlag = ClearFlags(_dirtyFlag, eLocalMatrix);
     }
     return _matWorld;
 }
@@ -74,7 +122,7 @@ auto Transform::GetWorldMatrix() const -> const glm::mat4x4 & {
             parentMatrix = _pParent->GetWorldMatrix();
         }
         _matWorld = GetLocalMatrix() * parentMatrix;
-        ClearFlags(_dirtyFlag, eWorldMatrix);
+        _dirtyFlag = ClearFlags(_dirtyFlag, eWorldMatrix);
     }
     return _matWorld;
 }
@@ -82,17 +130,32 @@ auto Transform::GetWorldMatrix() const -> const glm::mat4x4 & {
 auto Transform::GetInverseLocalMatrix() const -> const glm::mat4x4 & {
     if (HasFlag(_dirtyFlag, eInverseLocalMatrix)) {
         _matInvLocal = inverse(GetLocalMatrix());
-        ClearFlags(_dirtyFlag, eInverseLocalMatrix);
+        _dirtyFlag = ClearFlags(_dirtyFlag, eInverseLocalMatrix);
     }
     return _matInvLocal;
 }
 
 auto Transform::GetInverseWorldMatrix() const -> const glm::mat4x4 & {
     if (HasFlag(_dirtyFlag, eInverseWorldMatrix)) {
-        _invMatWorld = inverse(GetWorldMatrix());
-        ClearFlags(_dirtyFlag, eInverseWorldMatrix);
+        _matInvWorld = inverse(GetWorldMatrix());
+        _dirtyFlag = ClearFlags(_dirtyFlag, eInverseWorldMatrix);
     }
-    return _invMatWorld;
+    return _matInvWorld;
+}
+
+auto Transform::GetWorldPosition() const -> const glm::vec3 & {
+    ConditionUpdateWorldAttribute();
+    return _worldTranslation;
+}
+
+auto Transform::GetWorldScale() const -> const glm::vec3 & {
+    ConditionUpdateWorldAttribute();
+    return _worldScale;
+}
+
+auto Transform::GetWorldRotation() const -> const glm::quat & {
+    ConditionUpdateWorldAttribute();
+    return _worldRotation;
 }
 
 void Transform::SetParentImpl(Transform *pParent, Transform *pChild) {
@@ -102,9 +165,24 @@ void Transform::SetParentImpl(Transform *pParent, Transform *pChild) {
 
 void Transform::RemoveChildImpl(Transform *pParent, Transform *pChild) {
     for (auto it = pParent->_children.begin(); it != pParent->_children.end(); ++it) {
-	    if (*it == pChild) {
+        if (*it == pChild) {
             pChild->_pParent = nullptr;
-		    pParent->_children.erase(it);
-	    }
+            pParent->_children.erase(it);
+        }
+    }
+}
+
+void Transform::ConditionUpdateWorldAttribute() const {
+    if (HasAnyFlags(_dirtyFlag, eWorldTranslation | eWorldScale | eWorldRotation)) {
+        glm::vec3 skew;
+        glm::vec4 perspective;
+        glm::decompose(GetWorldMatrix(), _worldScale, _worldRotation, _worldTranslation, skew, perspective);
+        _dirtyFlag = ClearFlags(_dirtyFlag, eWorldTranslation | eWorldScale | eWorldRotation);
+    }
+}
+
+void Transform::MakeChildrenDirty(TransformDirtyFlag flag) {
+    for (auto *pChild : _children) {
+        pChild->_dirtyFlag = SetFlags(pChild->_dirtyFlag, flag);
     }
 }
--- Runtime/Components/Transform.h
+++ Runtime/Components/Transform.h
@@ -8,22 +8,29 @@ class Transform : public Component {
     DECLARE_CLASS(Transform);
 public:
     static constexpr float kEpsilon = 0.00001f;
-    enum TransformDirtyFlag {
+    enum TransformDirtyFlag : uint32_t {
         // clang-format off
 		eLocalMatrix		= 1 << 0,
 		eWorldMatrix		= 1 << 1,
 		eInverseLocalMatrix = 1 << 2,
 		eInverseWorldMatrix = 1 << 3,
-        eAllDirty           = (eLocalMatrix | eWorldMatrix | eInverseLocalMatrix | eInverseWorldMatrix),
+        eWorldTranslation   = 1 << 4,
+        eWorldRotation      = 1 << 5,
+        eWorldScale         = 1 << 6,
+        eWorldAttribute     = (eWorldMatrix | eInverseWorldMatrix | eWorldTranslation | eWorldRotation | eWorldScale),
+        eAllDirty           = (~0),
         // clang-format on
     };
     ENUM_FLAGS_AS_MEMBER(TransformDirtyFlag);
 public:
     Transform();
+    ~Transform() override;
 public:
     void SetLocalPosition(const glm::vec3 &translate);
     void SetLocalScale(const glm::vec3 &scale);
-    void SetLocalRotate(const glm::quat &rotate);
+    void SetLocalRotation(const glm::quat &rotate);
+    void SetLocalMatrix(const glm::mat4x4 &matrix);
+    void SetWorldMatrix(const glm::mat4x4 &matrix);
     void SetParent(Transform *pTransform);
     void AddChild(Transform *pTransform);
     void RemoveChild(Transform *pTransform);
@@ -31,6 +38,9 @@ public:
     auto GetWorldMatrix() const -> const glm::mat4x4 &;
     auto GetInverseLocalMatrix() const -> const glm::mat4x4 &;
     auto GetInverseWorldMatrix() const -> const glm::mat4x4 &;
+    auto GetWorldPosition() const -> const glm::vec3 &;
+    auto GetWorldScale() const -> const glm::vec3 &;
+    auto GetWorldRotation() const -> const glm::quat &;
     auto GetLocalPosition() const -> const glm::vec3 & {
         return _translation;
     }
@@ -49,6 +59,8 @@ public:
 private:
     static void SetParentImpl(Transform *pParent, Transform *pChild);
     static void RemoveChildImpl(Transform *pParent, Transform *pChild);
+    void ConditionUpdateWorldAttribute() const;
+    void MakeChildrenDirty(TransformDirtyFlag flag);
 private:
     // clang-format off
 	glm::vec3					_translation;
@@ -58,10 +70,13 @@ private:
 	Transform				   *_pParent;
 	std::vector<Transform *>	_children;
 
+    mutable glm::vec3			_worldTranslation;
+	mutable glm::quat			_worldRotation;
+	mutable glm::vec3			_worldScale;
 	mutable glm::mat4x4			_matLocal;
 	mutable glm::mat4x4			_matWorld;
 	mutable glm::mat4x4			_matInvLocal;
-	mutable glm::mat4x4			_invMatWorld;
+	mutable glm::mat4x4			_matInvWorld;
 	mutable TransformDirtyFlag	_dirtyFlag;
     // clang-format on
 };
